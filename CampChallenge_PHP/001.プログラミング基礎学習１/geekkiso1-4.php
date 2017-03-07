0159eff72ded78fe94f8413e8d09251edb8fa896
雨宮　法広 <kemunpus-de-yans@kfd.biglobe.ne.jp>
2017-03-07 11:53:43 +0900

    課題　四則演算

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/geekkiso1-4.php
@@ -0,0 +1,11 @@
+<?php
+//変数、定数を宣言
+$x = "8";
+const Y = "14";
+//四則演算を行い、結果を表示する
+$hiku = $x - Y;
+ $tasu = $x + Y;
+$kake = $x * Y;
+$amari = $x % Y;
+//結果表示
+echo $tasu . "<br>" . $hiku . "<br>" . $kake . "<br>" . $amari;
