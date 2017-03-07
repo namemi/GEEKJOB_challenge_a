336fe3756be4e99bd03dcb2a23d11b29f3f50cbb
雨宮　法広 <kemunpus-de-yans@kfd.biglobe.ne.jp>
2017-03-07 16:37:14 +0900

    課題"switch文１"

---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/geekkiso2-1.php
@@ -0,0 +1,15 @@
+<?php
+  $num = -99;
+  $message = '';
+  switch($num){
+    case 1:
+    $message = 'one';
+    break;
+    case 2:
+    $message = 'two';
+    break;
+    default:
+    $message = '想定外';
+    break;
+  }
+  echo $message;
